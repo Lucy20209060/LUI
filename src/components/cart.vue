82390a9fc92fe476e7ba625d09a6f542a5060a7e
luc <luc@hzanchu.com>
2017-08-16 18:50:46 +0800

    message

--- src/components/cart.vue
+++ src/components/cart.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="hello-wrap">
-        <lu_button @click="tap">大超哥</lu_button>
+        <lu_button @click="tap0">大超哥</lu_button>
+        <lu_button @click="tap1">大超哥</lu_button>
+        <lu_button @click="tap2">大超哥</lu_button>
+        <lu_button @click="tap3">大超哥</lu_button>
     </div>
 </template>
 
@@ -15,11 +18,10 @@ export default {
         }
     },
     mounted(){
-    	
 
     },
     methods:{
-        tap(){
+        tap0(){
             this.$lu_messagebox({
           title: '消息',
           message: '这是一段内容, 这是一段内容, 这是一段内容, 这是一段内容, 这是一段内容, 这是一段内容, 这是一段内容',
@@ -47,7 +49,76 @@ export default {
           // });
         console.log(1111)
         });
-        }
+        },
+
+        tap1() {
+        this.$lu_messagebox.alert('这是一段内容', '标题名称', {
+          confirmButtonText: '确定',
+          callback: action => {
+            // this.$message({
+            //   type: 'info',
+            //   message: `action: ${ action }`
+            // });
+          }
+        });
+      },
+
+
+      tap2() {
+        this.$lu_messagebox.confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          // this.$message({
+          //   type: 'success',
+          //   message: '删除成功!'
+          // });
+        }).catch(() => {
+          // this.$message({
+          //   type: 'info',
+          //   message: '已取消删除'
+          // });          
+        });
+      },
+
+
+
+
+      tap3() {
+        this.$lu_messagebox({
+          title: '消息',
+          message: '这是一段内容, 这是一段内容, 这是一段内容, 这是一段内容, 这是一段内容, 这是一段内容, 这是一段内容',
+          showCancelButton: true,
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          beforeClose: (action, instance, done) => {
+            if (action === 'confirm') {
+              instance.confirmButtonLoading = true;
+              instance.confirmButtonText = '执行中...';
+              setTimeout(() => {
+                done();
+                setTimeout(() => {
+                  instance.confirmButtonLoading = false;
+                }, 300);
+              }, 3000);
+            } else {
+              done();
+            }
+          }
+        }).then(action => {
+          // this.$message({
+          //   type: 'info',
+          //   message: 'action: ' + action
+          // });
+        });
+      },
+
+
+
+
+
+
 
     	
 	}
