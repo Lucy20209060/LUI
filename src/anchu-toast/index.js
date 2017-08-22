935de3b099adc9c76917b3fe33d502b7e44d0074
Lucy20209060 <you@example.com>
2017-08-22 21:28:58 +0800

    lu

--- src/anchu-toast/index.js
+++ src/anchu-toast/index.js
@@ -1,17 +1,7 @@
-import lu_button from './button';
-import lu_radio from './radio';
-import lu_badge from './badge';
-import lu_toast from './toast';
-import lu_switch from './switch';
 import lu_message from './message';
 import lu_messagebox from './message-box';
 
 const components = [
-	lu_button,
-  lu_radio,
-  lu_badge,
-  lu_toast,
-  lu_switch,
   lu_message
 ]
 
@@ -22,10 +12,7 @@ const install = function(Vue) {
     Vue.component(component.name, component);
   });
 
-
-  Vue.prototype.$lu_toast = lu_toast;
   Vue.prototype.$lu_message = lu_message;
-  Vue.prototype.$lu_messagebox = lu_messagebox;
 
 };
 
@@ -36,11 +23,5 @@ if (typeof window !== 'undefined' && window.Vue) {
 
 export default {
 	install,
-	lu_button,
-  lu_radio,
-  lu_badge,
-  lu_toast,
-  lu_switch,
-  lu_message,
-  lu_messagebox
+  lu_message
 }
