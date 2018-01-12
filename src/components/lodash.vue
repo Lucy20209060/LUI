0579828323cbcbe36c633fecb4ac40d6cea6604d
luc <550502661@qq.com>
2018-01-13 02:00:02 +0800

    compact剔除掉数组中的假值（null,'',undefined,NaN）

--- src/components/lodash.vue
+++ src/components/lodash.vue
@@ -18,12 +18,19 @@ export default {
             chunk
             将某个数组按特定的长度划分成多个数组
         */
-        const arr1 = _.chunk(['a', 'b', 'c', 'd',1], 2) //[['a','b'],['c','d'],[1]]
+        const arr1 = _.chunk(['a', 'b', 'c', 'd',1], 2) 
+        //[['a','b'],['c','d'],[1]]
+
+        /*
+            compact
+            剔除掉数组中的假值（null,'',undefined,NaN）
+            " "空字符串 不是假值 
+        */
+        const arr2 = _.compact([false,null,true,0,'',' ',undefined,NaN,'a',1])
+        // [true, " ", "a", 1]
 
 
-
-
-        console.log(arr1)
+        console.log(arr2)
     },
     methods:{
         interval(){
